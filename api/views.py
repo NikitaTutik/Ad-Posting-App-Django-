f2caa62ebd6aee2d2ecf8bb690ace907a6235d38
NikitaTutik <famousnt@gmail.com>
2021-02-01 15:44:54 +0300

    API pagination and filters

--- api/views.py
+++ api/views.py
@@ -1,8 +1,11 @@
 from rest_framework import viewsets
 from .models import Ad
 from .serializers import AdSerializer
+import django_filters.rest_framework
 
 
 class AdViewset(viewsets.ModelViewSet):
     queryset = Ad.objects.all()
     serializer_class = AdSerializer
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
+    filterset_fields = ['ad_category', 'author']
--- djangoProject2/settings.py
+++ djangoProject2/settings.py
@@ -130,3 +130,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = 'index'
 LOGIN_URL = 'login'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5,
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
