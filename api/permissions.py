f1bdc39560f3ae3f24be5c6d1c5223d7112acbfe
NikitaTutik <famousnt@gmail.com>
2021-02-08 16:52:30 +0300

    fixed group based permissions

--- api/permissions.py
+++ api/permissions.py
@@ -1,9 +1,13 @@
+from django.contrib.auth.models import Group
 from rest_framework import permissions
 
 
 class UserPermission(permissions.BasePermission):
 
     def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return bool(Group.objects.get(name='SuperUser').
+                        user_set.filter(id=request.user.id).exists())
         if view.action == 'list':
             return request.user.is_authenticated and request.user.is_staff
         elif view.action == 'create':
@@ -13,10 +17,10 @@ class UserPermission(permissions.BasePermission):
         else:
             return False
 
+
     def has_object_permission(self, request, view, obj):
         if not request.user.is_authenticated:
             return False
-
         if view.action == 'retrieve':
             return obj == request.user or request.user.is_staff
         elif view.action in ['update', 'partial_update']:
