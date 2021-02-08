cf7b41740b04d082b33075992c8571e3c32065b6
NikitaTutik <famousnt@gmail.com>
2021-02-08 20:04:54 +0300

    extended phone number validation

--- api/serializers.py
+++ api/serializers.py
@@ -8,11 +8,17 @@ class AdSerializer(ModelSerializer):
     author = serializers.HiddenField(default=serializers.CurrentUserDefault())
     phone_number = serializers.IntegerField()
 
+    def validate_phone_number(self, value):
+        value = self.initial_data.get('phone_number')
+        if len(value) == 11:
+            return value
+        raise serializers.ValidationError('Length of the phone number should be 11 symbols')
+
     def validate_true_int(self, value):
         value = self.initial_data.get('phone_number')
         if isinstance(value, int):
             return value
-        raise ValidationError("Not an int")
+        raise serializers.ValidationError("Not an int")
 
     class Meta:
         model = Ad
